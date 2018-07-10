429663a4cbae2527c042adcb181b9647a719dcfc
masrin123@hotmail.com <masrin123@hotmail.com>
2018-07-10 13:03:02 -0700

    added classes for the days

--- init.py
+++ init.py
@@ -1,61 +1,90 @@
-import random
+from random import randint
 import requests
 import smtplib
 from bs4 import BeautifulSoup
 
+
+# Shirts
 t_shirts = ['blue vineyard vines', 'pink vineyard vines', 'blue riot' ]
 long_shirts = ['button-1', 'button-2', 'button-3']
 
+shirts = [t_shirts, long_shirts]
 
-pants = ['khakis-1', 'khakis-2']
+# Pants
+slacks = ['khakis-1', 'khakis-2']
 shorts = ['black vans', 'vineyard shorts', 'eagle shorts']
 
+pants = [shorts, slacks]
+
+# Shoes
 shoes = ['sambas', 'sperrys', 'nmd']
+shoe_choices = shoes.copy()
 
+# Optional Accessories
 belt = False
+sweater = True
 
-def temperature():
-    page = requests.get('https://weather.com/weather/tenday/l/Menlo+Park+CA+94025:4:US')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    forecast_items = soup.find_all('td', class_="temp")
-    temp = []
-    for forecast in forecast_items[1:6]:
-        temp.append(forecast.select("div")[0].get_text())
-    return temp
+# Scraping the Website for the Weekday Forecast
+page = requests.get('https://weather.com/weather/tenday/l/Menlo+Park+CA+94025:4:US')
+soup = BeautifulSoup(page.content, 'html.parser')
+forecast_items = soup.find_all('td', class_="temp")
+temp = []
+for forecast in forecast_items[1:6]:
+    temp.append(forecast.select("div")[0].get_text())
 
+forecast = temp
 
-forecast = temperature()
+# Storing the forecast in each day
 weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
 
-forecast_dict = {}
+class report:
+    def __init__(self, day):
+        self.high = int(forecast[weekdays.index(day)][0:2])
+        self.low = int(forecast[weekdays.index(day)][3:5])
+    def get_average(self):
+        avg = (self.low + self.high)/2
+        return avg
+    def clothes(self, top, bottom, shoe, sweater, belt):
+        return (top, bottom, shoe, sweater, belt)
+
 for day in weekdays:
-    forecast_dict["{0}_high".format(day)] = int(forecast[weekdays.index(day)][0:2])
-    forecast_dict["{0}_low".format(day)] = int(forecast[weekdays.index(day)][3:5])
-
-monday_average = (forecast_dict['monday_high'] + forecast_dict['monday_low']) / 2
-tuesday_average = (forecast_dict['tuesday_high'] + forecast_dict['tuesday_low']) / 2
-wednesday_average = (forecast_dict['wednesday_high'] + forecast_dict['wednesday_low']) / 2
-thursday_average = (forecast_dict['thursday_high'] + forecast_dict['thursday_low']) / 2
-friday_average = (forecast_dict['friday_high'] + forecast_dict['friday_low']) / 2
-
-averages_dict = {
-    'monday': monday_average,
-    'tuesday': tuesday_average,
-    'wednesday': wednesday_average,
-    'thursday': thursday_average,
-    'friday': friday_average
-}
+    day = report(day)
 
 output_file = open('message.txt', 'w')
 output_file.write('\n\n')
-for average in averages_dict:
-    if averages_dict[average] >= 75 or forecast_dict['{}_high'.format(average)] >= 80:
-        output_file.write('{}:\n\tWork: wear shorts and a short sleeve shirt'.format(average))
-    else:
-        output_file.write('{}:\n\tWork: wear long pants and a long sleeve shirt'.format(average))
+for day in weekdays:
+    if day.get_average() >= 75 or day.high >= 80:
+        try:
+            bottom = pants[0].pop(randint(0,len(pants[0])))
+        except IndexError:
+            pants = [shorts, slacks]
+            pants.remove(weekdays(weekdays.index(day)-1).clothes[1])
+            bottom = pants[0].pop(randint(0,len(pants[0])))
 
-    if forecast_dict['{}_low'.format(average)] <= 58 and average in ('monday', 'wednesday', 'friday'):
-        output_file.write('\n\tbring a sweater')
+
+        try:
+            bottom = pants[0].pop(randint(0,len(pants[0])))
+        except IndexError:
+            pants = [shorts, slacks]
+            pants.remove(weekdays(weekdays.index(day)-1).clothes[1])
+            bottom = pants[0].pop(randint(0,len(pants[0])))
+    else:
+        try:
+            bottom = pants[1].pop(randint(0,len(pants[1])))
+        except IndexError:
+            pants = [shorts, slacks]
+            pants.remove(weekdays(weekdays.index(day)-1).clothes[1])
+            bottom = pants[1].pop(randint(0,len(pants[1])))
+
+        try:
+            bottom = pants[1].pop(randint(0,len(pants[1])))
+        except IndexError:
+            pants = [shorts, slacks]
+            pants.remove(weekdays(weekdays.index(day)-1).clothes[1])
+            bottom = pants[1].pop(randint(0,len(pants[1])))
+
+    if day.low <= 58 and day in ('monday', 'wednesday', 'friday'):
+        sweater = True
 
 
     output_file.write('\n')
